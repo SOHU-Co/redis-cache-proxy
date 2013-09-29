ee4094fdb60a03f55e67432063b60d36fbdf43f6
zhongyu <yuzhong@sohu-inc.com>
2013-09-29 06:37:23 -0400

    refactor

--- feed.js
+++ feed.js
@@ -1,7 +1,9 @@
 var hiredis = require('hiredis');
 
 var reader = new hiredis.Reader();
-reader.feed('*1\r\n$3\r\nget\r\n');
+reader.feed('*1\r\n$3\r\n');
+
+reader.feed('get\r\n');
 console.log(reader.get());
 console.log(reader.get());
 reader.feed('*1\r\n$3\r\nget\r\n');
